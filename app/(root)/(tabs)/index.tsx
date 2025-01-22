9bf0a015b1158336e3d4ba34624bfd694d2d9876
Kyle Vance <vanceofalifetime@protonmail.com>
2025-01-21 17:47:32 -0800

    added nativewind, images, seed data

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
-import Explore from './explore';
 
 export default function Index() {
   return (
@@ -11,6 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="font-bold font-rubik text-3xl my-10">Welcome to this ReState</Text>
       <Link href={"/sign-in"}>Sign In</Link>
       <Link href={"/explore"}>Explore</Link>
       <Link href={"/profile"}>Profile</Link>
